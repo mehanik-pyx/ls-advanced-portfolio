31769c72cee62b332e52827723750355d19db5a4
Popov Alexandr <mehanik-pyx@mail.ru>
2020-02-22 06:10:37 +0700

    при клике на ссылку меню исчезает

--- src/scripts/popup-menu.js
+++ src/scripts/popup-menu.js
@@ -1,8 +1,19 @@
+const bodyTag = document.querySelector('body');
+
 (function (className) {
   const btn = document.querySelector(className);
-  const bodyTag = document.querySelector('body');
   
   btn.addEventListener('click', (e) => {
     bodyTag.classList.toggle('overlay');
   });
 })('.burger');
+
+(function (className) {
+  const linkNList = document.querySelectorAll(className);
+  Array.from(linkNList).forEach(link => {
+
+    link.addEventListener('click', (e) => {
+    bodyTag.classList.remove('overlay');
+    });
+  });
+})('.nav__link');
