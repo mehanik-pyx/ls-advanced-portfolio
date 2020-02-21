7586cee28a210294686d358dbd08a170f373686b
mehanik-pyx <mehanik-pyx@mail.ru>
2020-02-21 08:06:43 +0700

    скиллы на вью и адаптив

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,59 @@
-// console.log('this is skills module');
+console.log('this is skills module');
+
+import Vue from "vue";
+
+const skill = {
+  template: "#skill",
+  props: {
+    skillName: String,
+    skillPercent: Number
+  },
+  methods: {
+    drawColoredCircle() {
+      const circle = this.$refs["color-circle"];
+      // const dashArray = parseInt(
+        // getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+      // );
+      // const percent = (dashArray / 100) * (100 - this.skillPercent);
+
+      // circle.style.strokeDashoffset = percent;
+      // console.log(skillPercent);
+      // console.log(this.strokeDashoffset);
+      // circle.style.strokeDashoffset = this.skillPercent + ' 100';
+      circle.style.strokeDasharray = this.skillPercent + ' 100';
+      // console.log(this.asd);
+      // console.log(asd);
+      // circle.style.strokeDasharray = asd; easyyyyyyy
+    }
+  },
+  mounted() {
+    this.drawColoredCircle();
+  }
+};
+
+const skillsRow = {
+  template: "#skills-row",
+  components: {
+    skill
+  },
+  props: {
+    skill: Object
+  }
+};
+
+new Vue({
+  el: "#skills-component",
+  template: "#skills-list",
+  components: {
+    skillsRow
+  },
+  data() {
+    return {
+      skills: {}
+    };
+  },
+  created() {
+    const data = require("../data/skills.json");
+    this.skills = data;
+  }
+});
